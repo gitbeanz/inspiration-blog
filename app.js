7e419b17ae2ab29193a854a807856e1e6ad40665
gitbeanz <74123124+gitbeanz@users.noreply.github.com>
2022-06-29 19:45:53 -0700

    worked on movie carousel.

--- app.js
+++ app.js
@@ -3,3 +3,34 @@ const links = document.getElementsByClassName('links')[0]
 toggleButton.addEventListener('click',()=>{
     links.classList.toggle('active');
 })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(slidePage){
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (slidePage > slides.length){
+        slideIndex = 1;
+    }
+    if (slidePage < 1){
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for (i=0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
